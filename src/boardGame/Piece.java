a06797f853094046ece210010d1059cb53948a0f
DidoMota <didomota.88@gmail.com>
2021-10-04 10:56:43 -0300

    Possible moves of a piece

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -1,6 +1,6 @@
-package boardGame;
+ package boardGame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;		
 	private Board board;
@@ -18,4 +18,29 @@ public class Piece {
 		return board;
 	}
 	
+	//methods
+	public abstract boolean[][] possibleMoves();
+	
+	//esse método concreto utiliza o método abstrato, hook methods
+	//só é possível o método ser concreto, pq ele chama uma implementação concreta de uma classe abstrata
+	public boolean possibleMove(Position position) {						
+		return possibleMoves()[position.getRow()][position.getColumn()]; 
+	}
+	
+	// o método verifica se há alguma possibilidade de movimento da peça, verifica se a peça não está travada
+	// implantação que depende de um método abstrato, hook methods
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for(int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 }
+
+//
